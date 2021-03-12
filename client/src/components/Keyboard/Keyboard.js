818d5440294bcc8e4caa1fc59c0d9f45608e6585
JanetChiu <jamchiu@gmail.com>
2021-03-12 15:09:49 -0500

    keyboard event handling

--- client/src/components/Keyboard/Keyboard.js
+++ client/src/components/Keyboard/Keyboard.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import './Keyboard.scss';
 
 function Keyboard(props) {
+  
+const clickHandler=(event) => {
+    if( event.key === props.key) {
+        return <li className="puzzle__letter" key={props.key} >{props.letter}</li>
+    }
+};
+
     let alphabetArr = ["a","b","c","d","e","f","g","h","i","j","k","l","m","o","p","q","r","s","t","u","v","w","x","y","z"]
     return (
         <div className="keyboard">
             {alphabetArr.map((letter)=> {
-                return <button type="submit" onCLick={props.clickHandler} giclassName="letter__box" key={letter}>{letter}</button>
+                return <button onCLick={clickHandler} className="keyboard__box" key={letter}>{letter}</button>
             })}
             
         </div>
