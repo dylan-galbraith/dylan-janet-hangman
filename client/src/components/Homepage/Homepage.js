38c903df748888559586b18098f38dfa1eb442f9
JanetChiu <jamchiu@gmail.com>
2021-03-12 13:57:20 -0500

    preliminary keyboard done

--- client/src/components/Homepage/Homepage.js
+++ client/src/components/Homepage/Homepage.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Hangman from "../Hangman/Hangman";
+import Keyboard from "../Keyboard/Keyboard";
 
 class Homepage extends Component {
     render() {
         return (
             <div>
                 <Hangman />
+                <Keyboard />
                 
             </div>
         );
--- client/src/components/Keyboard/Keyboard.js
+++ client/src/components/Keyboard/Keyboard.js
@@ -1,5 +1,15 @@
 import React from 'react';
 
-
+function Keyboard(props) {
+    let alphabetArr = ["a","b","c","d","e","f","g","h","i","j","k","l","m","o","p","q","r","s","t","u","v","w","x","y","z"]
+    return (
+        <div className="keyboard">
+            {alphabetArr.map((letter)=> {
+                return <button type="submit" className="letter__box" key={letter}>{letter}</button>
+            })}
+            
+        </div>
+    );
+}
 
 export default Keyboard;
